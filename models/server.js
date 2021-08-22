ff7c2a6f21fedbd4dedd88334779956af873db2d
John Doe <arnolhhh@gmail.com>
2021-08-22 09:44:16 -0500

    cors

--- models/server.js
+++ models/server.js
@@ -3,7 +3,7 @@ const express  = require('express');
 const http     = require('http');
 const socketio = require('socket.io');
 const path     = require('path');
-//const cors     = require('cors');
+const cors     = require('cors');
 
 const Sockets  = require('./sockets');
 
@@ -27,7 +27,7 @@ class Server {
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
 
         // CORS
-        //this.app.use( cors() );
+        this.app.use( cors() );
 
     }
 
